207691cffeb3402c082872b9e5a25c51d05df61f
DIBYA9 <77687274+DIBYA9@users.noreply.github.com>
2021-06-15 20:00:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center():
+}
+
+function draw() {
+    i
+}
+
+
+function take_snapshot() {
+    save('mustache.png')
+}
